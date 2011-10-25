0aacab55c4dd717624cc92dc1a67ccedc86b9aae
Bruno Basto <bruno.basto@liferay.com>
2011-10-25 19:02:46 -0300

    Removiong white spaces

--- src/com/ql/parser/ReservedWords.java
+++ src/com/ql/parser/ReservedWords.java
@@ -3,7 +3,7 @@ package com.ql.parser;
 public class ReservedWords {
 
 	public static final String OR = "OR";
-	
+
 	public static final String AND = "AND";
-	
+
 }
--- src/com/ql/parser/TokenType.java
+++ src/com/ql/parser/TokenType.java
@@ -3,9 +3,9 @@ package com.ql.parser;
 public enum TokenType {
 
 	STRING,
-	
+
 	L_PARENT,
-	
+
 	R_PARENT,
 
 	OPERATOR,
